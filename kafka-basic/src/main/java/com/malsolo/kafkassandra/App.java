dbc32b6e29a96d56d0ed38c69cffc5c64b917312
Javier Beneito Barquero <malsolo.com@outlook.com>
2020-10-13 14:43:32 +0200

    kafkassandra: kafka basic module from maven archetype.

---
+++ kafka-basic/src/main/java/com/malsolo/kafkassandra/App.java
@@ -0,0 +1,17 @@
+package com.malsolo.kafkassandra;
+
+import java.util.stream.IntStream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        IntStream.range(1, 6)
+            .filter(i -> i % 2 == 0)
+            .mapToObj(Integer::toString)
+            .map(s -> String.format("Hi %s", s))
+            .forEach(System.out::println);
+    }
+}
