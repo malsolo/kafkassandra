917aaa5ba461b85334ff02b048b7745323be2c22
Javier Beneito Barquero <malsolo.com@outlook.com>
2020-12-30 16:21:24 +0100

    kafkassandra: cassandra boot, test for Reactive Repository implemented with SimpleReactiveCassandraRepository.

--- cassandra-boot/src/main/java/com/malsolo/kafkassandra/cassandra/boot/repository/StockSearchRepository.java
+++ cassandra-boot/src/main/java/com/malsolo/kafkassandra/cassandra/boot/repository/StockSearchRepository.java
@@ -1,18 +1,75 @@
 package com.malsolo.kafkassandra.cassandra.boot.repository;
 
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.bindMarker;
+import static org.springframework.data.cassandra.core.query.Criteria.where;
+import static org.springframework.data.cassandra.core.query.Query.query;
+
+import com.datastax.oss.driver.api.core.CqlIdentifier;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
+import com.datastax.oss.driver.api.querybuilder.relation.Relation;
+import java.time.Instant;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.cassandra.core.ReactiveCassandraOperations;
 import org.springframework.data.cassandra.repository.query.CassandraEntityInformation;
 import org.springframework.data.cassandra.repository.support.SimpleReactiveCassandraRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public class StockSearchRepository extends SimpleReactiveCassandraRepository<Stock, StockKey> {
+
+    private static final CqlIdentifier STOCKS = CqlIdentifier.fromCql("stocks");
+
+    public static final CqlIdentifier SYMBOL = CqlIdentifier.fromCql("symbol");
+    public static final CqlIdentifier DATE = CqlIdentifier.fromCql("date");
+    public static final CqlIdentifier VALUE = CqlIdentifier.fromCql("value");
+
+    private static final CqlIdentifier START = CqlIdentifier.fromCql("start");
+    private static final CqlIdentifier END = CqlIdentifier.fromCql("end");
+
+    public static final SimpleStatement FIND_STOCKS_BY_SYMBOL = QueryBuilder.selectFrom(STOCKS)
+        .columns(SYMBOL, DATE, VALUE)
+        .where(
+            Relation.column(SYMBOL).isEqualTo(bindMarker(SYMBOL)),
+            // start inclusive
+            Relation.column(DATE).isGreaterThanOrEqualTo(bindMarker(START)),
+            // end exclusive
+            Relation.column(DATE).isLessThan(bindMarker(END))
+        )
+        .build();
+
+    private final ReactiveCassandraOperations operations;
 
     public StockSearchRepository(
         @Qualifier("stockCassandraEntityInformation")
         CassandraEntityInformation<Stock, StockKey> metadata,
         ReactiveCassandraOperations operations) {
         super(metadata, operations);
+        this.operations = operations;
+    }
+
+    public Flux<Stock> searchBySymbol(
+        @NonNull String symbol,
+        @NonNull Instant start,
+        @NonNull Instant end,
+        long offset,
+        long limit) {
+
+        /*
+        FIND_STOCKS_BY_SYMBOL.setNamedValues(Map.of(
+            SYMBOL.asInternal(), symbol,
+            START.asInternal(), start,
+            END.asInternal(), end
+        )).getQuery();
+
+         */
+
+        var stockFlux = operations.select(query(where("symbol").is(symbol))
+            .and(where("date").gte(start))
+            .and(where("date").lt(end)), Stock.class);
+
+        return stockFlux.skip(offset).take(limit);
     }
 }
--- cassandra-boot/src/test/java/com/malsolo/kafkassandra/cassandra/boot/repository/StockSearchRepositoryTest.java
+++ cassandra-boot/src/test/java/com/malsolo/kafkassandra/cassandra/boot/repository/StockSearchRepositoryTest.java
@@ -2,9 +2,12 @@ package com.malsolo.kafkassandra.cassandra.boot.repository;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 @SpringBootTest
@@ -30,7 +33,6 @@ public class StockSearchRepositoryTest {
             .expectNextMatches(stockSaved -> stockKey.equals(stockSaved.getKey()))
             .verifyComplete();
 
-
         //When
         var stockById = stockSearchRepository.findById(stockKey);
         //Then
@@ -44,6 +46,56 @@ public class StockSearchRepositoryTest {
         var deleted = stockSearchRepository.delete(stock);
         //Then
         StepVerifier.create(deleted)
+            .expectNextCount(0)
+            .verifyComplete();
+    }
+
+    @Test
+    void should_return_paged_flux_when_searching_by_symbol() {
+        var symbol = "FIND";
+        var offset = 10;
+        var limit = 5;
+
+        var now = LocalDateTime.now()
+            .withHour(12).withMinute(0).withSecond(0).withNano(0);
+        var to = now.plusMinutes(2);
+
+        var zone = ZoneId.systemDefault();
+
+        var stockFlux = Flux.range(0, 20)
+            .map(now::withSecond)
+            .map(ldt ->
+                new Stock(
+                    new StockKey(symbol, ldt.atZone(zone).toInstant()),
+                    BigDecimal.valueOf(ldt.getSecond()))
+            );
+
+        var stockFluxSaved = stockFlux
+            .flatMap(stockSearchRepository::save);
+
+        StepVerifier.create(stockFluxSaved)
+            .expectNextCount(20)
+            .verifyComplete();
+
+        var stockFluxFound = stockSearchRepository.searchBySymbol(symbol,
+            now.atZone(ZoneId.systemDefault()).toInstant(),
+            to.atZone(ZoneId.systemDefault()).toInstant(),
+            offset, limit);
+
+        StepVerifier.create(stockFluxFound)
+            .expectNextMatches(stock ->
+                symbol.equals(stock.getKey().getSymbol()) &&
+                now.plusSeconds(offset - 1).equals(LocalDateTime.ofInstant(stock.getKey().getDate(), zone)))
+            .expectNextCount(limit - 2)
+            .expectNextMatches(stock ->
+                symbol.equals(stock.getKey().getSymbol()) &&
+                now.plusSeconds(offset - limit).equals(LocalDateTime.ofInstant(stock.getKey().getDate(), zone)))
+            .verifyComplete();
+
+        var stockFluxDeleted = stockFlux
+            .flatMap(stockSearchRepository::delete);
+
+        StepVerifier.create(stockFluxDeleted)
             .expectNextCount(0)
             .verifyComplete();
     }
